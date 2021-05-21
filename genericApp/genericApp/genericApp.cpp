a587496fd09deaa0dc4b193eb3e97956b7702181
Riomh Marsh <270121842@yoobeestudent.ac.nz>
2021-05-21 16:22:02 +1200

    initial commit

---
+++ genericApp/genericApp/genericApp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    //Riomh's Code
+    //============
+    int bottles, i;
+    cout << "How many bottles?\t";
+    cin >> bottles;
+
+    for (i = bottles; i > 0; i--) {
+        cout << i << " bottles of beer on the wall, " << i << " bottles of beer. Take one down, drink it down, " << i - 1 << " bottles of beer on the wall\n";
+    }
+    //============
+}
